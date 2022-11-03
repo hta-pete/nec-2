a89c8a44b8b4db0d4a4690e302e9fd5b0424b3aa
hta-pete <pkatsaboulas@hightoweragency.com>
2022-11-03 11:22:24 -0500

    initial commit

--- js/app.js
+++ js/app.js
@@ -1,5 +1,12 @@
+// init LocomotiveScroll on page load
+let scroller = new LocomotiveScroll({
+  el: document.querySelector('[data-scroll-container]'),
+  smooth: true
+});
+
 // tell Barba to use the css plugin
 barba.use(barbaCss);
+
 
 // init Barba
 barba.init({
@@ -56,61 +63,10 @@ barba.init({
 });
 
 
-
-$(function(){
-  /*
-  const scroller = new LocomotiveScroll({
-    el: document.querySelector('[data-scroll-container]'),
-    smooth: true,
-    getDirection: true
-  });
-
-
-  scroller.on('scroll', (instance) => {
-    document.documentElement.setAttribute('data-direction', instance.direction)
-  });
-
-  */
-
-	var $window = $(window);
-	var raf           = requestAnimationFrame;
-  var lastScrollTop = $('main').offset().top;
-
-  $window.on('load', function(){
-    window.dispatchEvent(new Event('resize'));
-  });
-
-  if (raf) {
-    loop();
-  }
-
-  function loop() {
-
-    var scrollTop = $('main').offset().top;
-    var y         = (scrollTop > lastScrollTop) ? 'down' : ((scrollTop === lastScrollTop) ? 'none' : 'up');
-
-    if (lastScrollTop === scrollTop) {
-      raf(loop);
-      return;
-    } else {
-      lastScrollTop = scrollTop;
-      raf(loop);
-    }
-
-    lastScrollTop = scrollTop;
-
-    if (scrollTop < -50){
-      $("header").addClass("active");
-    } else{
-      $("header").removeClass("active");
-    }
-   
-  }
-
-    
+// update the scroll after entering a page
+barba.hooks.after(() => {
+  scroller.update();
 });
-
-
 
 
 
